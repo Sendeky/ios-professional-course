cb62c240d78b23489846d27779d516a65b93b081
Ruslan Spirkin <83136978+Sendeky@users.noreply.github.com>
2022-09-07 10:06:56 -0700

    Fixed a typo

--- Bankey/Bankey/Utils/UIVIewController + Utils.swift
+++ Bankey/Bankey/Utils/UIVIewController + Utils.swift
@@ -9,12 +9,12 @@ import UIKit
 
 extension UIViewController {
     func setStatusBar() {
-        let statusBarSize = UIApplication.shared.statusBarFrame.size                    //Deprecated but ok
-        let frame = CGRect(origin: .zero, size: statusBarSize)
-        let statusBarView = UIView(frame: frame)
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground()       //Also hides navigation bar line
+        navBarAppearance.backgroundColor = appColor
         
-        statusBarView.backgroundColor = appColor
-        view.addSubview(statusBarView)
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
     }
     
     func setTabBarImage(imageName: String, title: String) {
